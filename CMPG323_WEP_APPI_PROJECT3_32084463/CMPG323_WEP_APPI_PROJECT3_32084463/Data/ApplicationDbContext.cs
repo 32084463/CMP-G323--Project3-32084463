99b1d08a25f6cad8263a2cf807a12a295236c517
32084463 <110498218+32084463@users.noreply.github.com>
2022-09-23 13:15:01 +0200

    initial commit

---
+++ CMPG323_WEP_APPI_PROJECT3_32084463/CMPG323_WEP_APPI_PROJECT3_32084463/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMPG323_WEP_APPI_PROJECT3_32084463.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
