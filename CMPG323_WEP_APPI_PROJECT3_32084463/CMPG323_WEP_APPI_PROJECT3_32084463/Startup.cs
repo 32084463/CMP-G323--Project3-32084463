7a06d85966a8578b227aafe8f83d0602eece6798
32084463 <110498218+32084463@users.noreply.github.com>
2022-09-23 13:43:33 +0200

    initial commit

--- CMPG323_WEP_APPI_PROJECT3_32084463/CMPG323_WEP_APPI_PROJECT3_32084463/Startup.cs
+++ CMPG323_WEP_APPI_PROJECT3_32084463/CMPG323_WEP_APPI_PROJECT3_32084463/Startup.cs
@@ -30,7 +30,7 @@ namespace CMPG323_WEP_APPI_PROJECT3_32084463
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
