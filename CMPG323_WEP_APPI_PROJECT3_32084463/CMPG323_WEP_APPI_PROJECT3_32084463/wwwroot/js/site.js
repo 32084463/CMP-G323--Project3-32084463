3e2297cdc3b75e59c3dece5b15d317529b38dd6a
32084463 <110498218+32084463@users.noreply.github.com>
2022-09-26 18:39:27 +0200

    initial commit

--- CMPG323_WEP_APPI_PROJECT3_32084463/CMPG323_WEP_APPI_PROJECT3_32084463/wwwroot/js/site.js
+++ CMPG323_WEP_APPI_PROJECT3_32084463/CMPG323_WEP_APPI_PROJECT3_32084463/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
